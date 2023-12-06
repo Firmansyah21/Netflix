3187d2e67804102cdebd5fb9550204de8dbce04f
syahfirman21 <syahfirman2106@gmail.com>
2023-12-06 13:34:41 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1A1C22",
+        putih: "#ffff",
+        button: "#BC0E19",
+        red: "#DB202C",
+      },
+    },
+  },
+  plugins: [],
+};
